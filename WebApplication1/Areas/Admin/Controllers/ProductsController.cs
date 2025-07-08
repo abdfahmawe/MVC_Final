85d3e99726f2472069052defbafe626582643157
abdfahmawe <fahmawe09@gmail.com>
2025-07-08 22:45:18 +0300

    add the update and delete to products

--- WebApplication1/Areas/Admin/Controllers/ProductsController.cs
+++ WebApplication1/Areas/Admin/Controllers/ProductsController.cs
@@ -41,5 +41,52 @@ namespace WebApplication1.Areas.Admin.Controllers
             }
             return View(requist);
         }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            ViewBag.CategoryId = context.categories
+            .Select(c => new SelectListItem
+                {
+             Value = c.Id.ToString(),
+                Text = c.Name
+                })
+                .ToList();
+            var products = context.products.Find(id);
+            if (products is not null)
+            {
+                return View(products);
+            }
+            return RedirectToAction("index");
+        }
+        [HttpPost]
+        public IActionResult Update(product requist)
+        {
+            ViewBag.CategoryId = context.categories
+          .Select(c => new SelectListItem
+          {
+              Value = c.Id.ToString(),
+              Text = c.Name
+          })
+              .ToList();
+            if (ModelState.IsValid)
+            {
+                context.products.Update(requist);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(requist);
+        }
+        public IActionResult Delete(int id)
+        {
+            var product = context.products.Find(id);
+            if (product is not null)
+            {
+                context.products.Remove(product);
+                context.SaveChanges();
+            }
+            return RedirectToAction("index");
+
+        }
+
     }
 }
